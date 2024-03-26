2156ede857f417ea16166724273c016050c04a6e
Subash Jirel <subashjrl@gmail.com>
2024-03-26 16:22:59 +0545

    created json web tokens when user logins with id and password

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,30 @@
-// const login = aync (req,res) => {
-//     res.send('Fake login/resgister/signup route')
-// }
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to fron-end
+// setup authentication so only the request with JWT can access the dasboard
+
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
-  res.send('Fake logn/register/signup route ');
+  const { username, password } = req.body;
+  // mongoose validation
+  // Joi
+  // check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+  //Just for demo, usually the id is provide by DB
+  const id = new Date().getDate();
+
+  //try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
