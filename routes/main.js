89299900715c1843cabd64d364f7b1cbd2e70dbf
Subash Jirel <subashjrl@gmail.com>
2024-03-26 19:54:53 +0545

    tansferred the code in dashboard route to authmiddleware

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleWare = require('../middleware/auth');
 
 const { login, dashboard } = require('../controllers/main');
 
-router.route('/dashboard').get(dashboard);
+router.route('/dashboard').get(authMiddleWare, dashboard);
 router.route('/login').post(login);
 
 module.exports = router;
